cee2bc90147a76dc7ee332b7c1f856f48e4eb5c1
NoussaibaBL <55873954+NoussaibaBL@users.noreply.github.com>
2019-11-26 10:11:01 +0100

    update

---
+++ js/paysvisite.js
@@ -0,0 +1,38 @@
+var listePaysVisites = [
+   
+   {
+     "name": "Algeria",
+     "url": "https://en.wikipedia.org/wiki/Algeria",
+     "lat": 28.033886,
+     "lng": 1.659626
+   },
+   {
+     "name": "France",
+     "url": "https://en.wikipedia.org/wiki/France",
+     "lat": 46.227638,
+     "lng": 2.213749
+   },
+   {
+     "name": "Spain",
+     "url": "https://en.wikipedia.org/wiki/Spain",
+     "lat": 40.463667,
+     "lng": -3.74922
+   },
+   
+   {
+     "name": "Germany",
+     "url": "https://en.wikipedia.org/wiki/Germany",
+     "lat": 51.165691,
+     "lng": 10.451526
+   },
+   
+   {
+     "name": "England",
+     "url": "https://en.wikipedia.org/wiki/England",
+     "lat": 52.355518,
+     "lng": -1.174320
+   },
+   
+   
+   
+];
